1f86a25fff8694870d10732d21fd7c340196ab34
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-10-08 22:52:43 -0300

    Problema 1871-Zero vale Zero-C#

---
+++ 1871/1871/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1871
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) {
+                String[] nums = Console.ReadLine().Split(' ');
+                int m = int.Parse(nums[0]);
+                int n = int.Parse(nums[1]);
+                if (m == 0 && n == 0) {
+                    break;
+                }
+                int soma = m + n;
+                string res = soma.ToString();
+                res = res.Replace("0","");
+                Console.WriteLine(res);
+
+            }
+        }
+    }
+}
